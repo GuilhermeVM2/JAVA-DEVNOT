f0832058c54e31d125f33dcc3fdbaa58431770fd
GuilhermeVM2 <123783825+GuilhermeVM2@users.noreply.github.com>
2023-06-15 21:53:24 -0300

    Add files via upload

--- POOBanco/App.java
+++ POOBanco/App.java
@@ -1,24 +1,51 @@
 package POOBanco;
 
 import java.util.Random;
+import java.util.Scanner;
 
 import javax.swing.JOptionPane;
 
 public class App {
     public static void main(String[] args) {
         Random rd = new Random();
-        //criar um array de objetos
-        TipoConta conta[] = new TipoConta[10];
-        //criar e preencher os objetos 
-        for (int i = 0; i < conta.length; i++) {
-            conta[i]=new TipoConta(); //construtor de objeto
-            //preencher
-            conta[i].setNome(JOptionPane.showInputDialog("Informe o Nome"));
+        // criar um array de objetos
+        TipoConta conta[] = new TipoConta[1];
+        // criar e preencher os objetos
+        boolean ligado = true;
+        Scanner sc = new Scanner(System.in);
+        int acao = 0;
+        while (ligado) {
+            for (int i = 0; i < conta.length; i++) {
             
+        int atuacao = Integer.parseInt(JOptionPane.showInputDialog("Escolha a ação desejada:\n"
+                    + "1. Criar Conta\n"
+                    + "2. Acessar Conta\n"
+                    + "3. Sair\n"));
 
+        switch (atuacao) {
+            case 1:
+                int opcao = Integer.parseInt(JOptionPane.showInputDialog("Escolha a ação desejada:\n"
+                    + "1. Pessoa Jurídica\n"
+                    + "2. Pessoa Física\n"
+                    + "3. Sair\n"));
 
+                    if (opcao == 1) {
+                        
+                    }
+
+                break;
+            case 2:
+                break;
+            case 3:
+            ligado=false;
+                break;
+            default:
+                JOptionPane.showMessageDialog( null,"Erro ");
+                break;
+        }  
         }
+    }
 
-       
+
     }
 }
--- POOBanco/TipoConta.java
+++ POOBanco/TipoConta.java
@@ -6,28 +6,30 @@ package POOBanco;
 public class TipoConta {
     String nome;
     int NDocumento;
-
-
+    int opcao;
 
     public String getNome() {
         return nome;
     }
 
-
     public void setNome(String nome) {
         this.nome = nome;
     }
-
 
     public int getNDocumento() {
         return NDocumento;
     }
 
-
     public void setNDocumento(int nDocumento) {
         NDocumento = nDocumento;
     }
-    
-    
-    
+
+    public int getOpcao() {
+        return opcao;
+    }
+
+    public void setOpcao(int opcao) {
+        this.opcao = opcao;
+    }
+        
 }
