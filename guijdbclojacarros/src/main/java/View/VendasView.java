7af1df25a7214e8f0dab2c3c3dbffb7d8a633c98
Vitor Andrade de Jesus <123784002+Vitorssauro@users.noreply.github.com>
2023-11-30 21:30:27 -0300

    30/11/2023
    implement cpf pesquisar

--- guijdbclojacarros/src/main/java/View/VendasView.java
+++ guijdbclojacarros/src/main/java/View/VendasView.java
@@ -1,82 +1,105 @@
 package View;
 
-import java.awt.GridLayout;
-import java.util.List;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
-import javax.swing.BoxLayout;
+import java.awt.GridLayout;
+
 import javax.swing.JButton;
 import javax.swing.JComboBox;
-import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-
-import Connection.CadastroDAO;
-import Controller.CarrosDAO;
-import Model.Cadastro;
-import Model.Carros;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.table.DefaultTableModel;
 
 public class VendasView extends JPanel {
-    JComboBox<String> carrosComboBox;
-    JComboBox<String> carrosComboBox1;
-    List<Carros> carros;
-    List<Cadastro> cadastros;
-    private JButton cadastrar;
+    private JButton procurar, adicionar, cadVenda;
+    private JTextField cadCpfField, totalField, totalDescField;
+    private DefaultTableModel tableModel;
+    private JTable table;
+    private JComboBox<String> codBarrasBox;
 
     public VendasView() {
         super();
-        carrosComboBox = new JComboBox<>();
-        // Preencha o JComboBox com os carros
-        carros = new CarrosDAO().listarTodos();
-        carrosComboBox.addItem("Selecione o Carro");
-        for (Carros carro : carros) {
-            carrosComboBox.addItem(carro.getMarca()
-                    + " " + carro.getModelo()
-                    + " " + carro.getPlaca());
-        }
 
-        add(carrosComboBox);
+        // add(new JLabel("Venda de Produto"));
+        JPanel inputPanel = new JPanel();
+        inputPanel.setLayout(new GridLayout(2, 2));
 
-        JComboBox carrosComboBox1 = new JComboBox<>();
-        // Preencha o JComboBox com os carros
-        cadastros = new CadastroDAO().listarTodos();
-        carrosComboBox1.addItem("Selecione o Cliente");
-        for (Cadastro cadastro : cadastros) {
-            carrosComboBox1.addItem(cadastro.getNome()
-                    + " " + cadastro.getCpf()
-                    + " " + cadastro.getidade());
-        }
-        add(carrosComboBox1);
-        //adiciona o botão cadastrar vendas
-        JPanel botoes = new JPanel();
-        botoes.add(cadastrar = new JButton("Cadastrar venda"));
-        add(botoes);
+        cadCpfField = new JTextField("Digite o CPF", 22);
+        inputPanel.add(cadCpfField);
 
+        inputPanel.add(procurar = new JButton("Procurar"));
 
+        // function botao
+        procurar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                pesquisarNoBanco();
+            }
+        });
+
+        // componentes Jcombobox
+        codBarrasBox = new JComboBox<>();
+        codBarrasBox.addItem("Cod. de Barras");
+        codBarrasBox.addItem("1");
+        codBarrasBox.addItem("2");
+        inputPanel.add(codBarrasBox);
+
+        inputPanel.add(adicionar = new JButton("Adicionar"));
+
+        add(inputPanel);
+
+        // tabela de compras
+        JScrollPane jSPane = new JScrollPane();
+        add(jSPane);
+        tableModel = new DefaultTableModel(new Object[][] {},
+                new String[] { "Item", "Qtd.", "$Un", "Preço Total" });
+        table = new JTable(tableModel);
+        jSPane.setViewportView(table);
+
+        JPanel submitPanel = new JPanel();
+        totalField = new JTextField("Valor total", 17);
+        submitPanel.add(totalField);
+        totalDescField = new JTextField("Total Desc.", 17);
+        submitPanel.add(totalDescField);
+
+        submitPanel.add(cadVenda = new JButton("Cad. Venda"));
+
+        add(submitPanel);
     }
 
-    public void atualizarComboBox() {
-        // Preencha o JComboBox com os carros
-        carros = new CarrosDAO().listarTodos();
-        carrosComboBox.removeAllItems();
-        carrosComboBox.addItem("Selecione o Carro");
-        for (Carros carro : carros) {
-            carrosComboBox.addItem(carro.getMarca()
-                    + " " + carro.getModelo()
-                    + " " + carro.getPlaca());
+    private void pesquisarNoBanco() {
+        String url = "jdbc:postgresql://localhost:5432/";
+        String user = "postgres";
+        String password = "postgres";
+        String valorPesquisado = cadCpfField.getText();
+
+        String sql = "SELECT * FROM cadastro_mercado WHERE codigo = ?";
+
+        try (
+                Connection connection = DriverManager.getConnection(url, user, password);
+                PreparedStatement preparedStatement = connection.prepareStatement(sql);) {
+            preparedStatement.setString(1, valorPesquisado);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+                JOptionPane.showMessageDialog(null, "foda");
+            } else {
+                JOptionPane.showMessageDialog(null, "Cliente ainda não cadastrado... :(");
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(null, "Erro ao pesquisar no banco de dados.");
         }
     }
-
-        public void atualizarComboBox1() {
-        // Preencha o JComboBox1 com os carros
-        cadastros = new CadastroDAO().listarTodos();
-        carrosComboBox1.removeAllItems();
-        carrosComboBox1.addItem("Selecione o Carro");
-        for (Cadastro carro : cadastros) {
-            carrosComboBox.addItem(carro.getNome()
-                    + " " + carro.getCpf()
-                    + " " + carro.getidade());
-        }
-    }
-
-
-
 }
