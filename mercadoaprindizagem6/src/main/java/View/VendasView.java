42498ce27b001652592650aba3255ce8431670cd
Vitor Andrade de Jesus <123784002+Vitorssauro@users.noreply.github.com>
2023-12-06 19:39:00 -0300

    Corrigir
    Commitei no guicarros ao invés do mercado e estou alterando para o mercado de novo

--- mercadoaprindizagem6/src/main/java/View/VendasView.java
+++ mercadoaprindizagem6/src/main/java/View/VendasView.java
@@ -1,9 +1,18 @@
 package View;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import java.awt.GridLayout;
 
 import javax.swing.JButton;
 import javax.swing.JComboBox;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
@@ -12,7 +21,7 @@ import javax.swing.table.DefaultTableModel;
 
 public class VendasView extends JPanel {
     private JButton procurar, adicionar, cadVenda;
-    private JTextField cadCpfField,totalField,totalDescField;
+    private JTextField cadCpfField, totalField, totalDescField;
     private DefaultTableModel tableModel;
     private JTable table;
     private JComboBox<String> codBarrasBox;
@@ -20,14 +29,22 @@ public class VendasView extends JPanel {
     public VendasView() {
         super();
 
-        //add(new JLabel("Venda de Produto"));
+        // add(new JLabel("Venda de Produto"));
         JPanel inputPanel = new JPanel();
-        inputPanel.setLayout(new GridLayout(2,2));
+        inputPanel.setLayout(new GridLayout(2, 2));
 
         cadCpfField = new JTextField("Digite o CPF", 22);
         inputPanel.add(cadCpfField);
 
         inputPanel.add(procurar = new JButton("Procurar"));
+
+        // function botao
+        procurar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                pesquisarNoBanco();
+            }
+        });
 
         // componentes Jcombobox
         codBarrasBox = new JComboBox<>();
@@ -59,4 +76,30 @@ public class VendasView extends JPanel {
         add(submitPanel);
     }
 
+    private void pesquisarNoBanco() {
+        String url = "jdbc:postgresql://localhost:5432/";
+        String user = "postgres";
+        String password = "postgres";
+        String valorPesquisado = cadCpfField.getText();
+
+        String sql = "SELECT * FROM cadastro_mercado WHERE codigo = ?";
+
+        try (
+                Connection connection = DriverManager.getConnection(url, user, password);
+                PreparedStatement preparedStatement = connection.prepareStatement(sql);) {
+            preparedStatement.setString(1, valorPesquisado);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+                JOptionPane.showMessageDialog(null, "foda");
+            } else {
+                JOptionPane.showMessageDialog(null, "Cliente ainda não cadastrado... :(");
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(null, "Erro ao pesquisar no banco de dados.");
+        }
+    }
 }
